ee353f304512576162ef5c63c131ce9207625a57
BaiHLiu <601179193@qq.com>
2023-08-16 10:50:20 +0800

    add lab1's exercise blank to lab5

--- labcodes/lab5/kern/init/init.c
+++ labcodes/lab5/kern/init/init.c
@@ -16,12 +16,12 @@
 
 int kern_init(void) __attribute__((noreturn));
 void grade_backtrace(void);
-static void lab1_switch_test(void);
 
 int
 kern_init(void) {
     extern char edata[], end[];
     memset(edata, 0, end - edata);
+
     cons_init();                // init the console
 
     const char *message = "(THU.CST) os is loading ...";
@@ -45,10 +45,6 @@ kern_init(void) {
     clock_init();               // init clock interrupt
     intr_enable();              // enable irq interrupt
 
-    //LAB1: CAHLLENGE 1 If you try to do it, uncomment lab1_switch_test()
-    // user/kernel mode switch test
-    //lab1_switch_test();
-    
     cpu_idle();                 // run idle process
 }
 
@@ -59,7 +55,7 @@ grade_backtrace2(int arg0, int arg1, int arg2, int arg3) {
 
 void __attribute__((noinline))
 grade_backtrace1(int arg0, int arg1) {
-    grade_backtrace2(arg0, (int64_t)&arg0, arg1, (int64_t)&arg1);
+    grade_backtrace2(arg0, (int)&arg0, arg1, (int)&arg1);
 }
 
 void __attribute__((noinline))
@@ -69,33 +65,12 @@ grade_backtrace0(int arg0, int arg1, int arg2) {
 
 void
 grade_backtrace(void) {
-    grade_backtrace0(0, (int64_t)kern_init, 0xffff0000);
+    grade_backtrace0(0, (int)kern_init, 0xffff0000);
 }
 
 static void
 lab1_print_cur_status(void) {
     static int round = 0;
     round ++;
-}
-
-static void
-lab1_switch_to_user(void) {
-    //LAB1 CHALLENGE 1 : TODO
-}
-
-static void
-lab1_switch_to_kernel(void) {
-    //LAB1 CHALLENGE 1 :  TODO
-}
-
-static void
-lab1_switch_test(void) {
-    lab1_print_cur_status();
-    cprintf("+++ switch to  user  mode +++\n");
-    lab1_switch_to_user();
-    lab1_print_cur_status();
-    cprintf("+++ switch to kernel mode +++\n");
-    lab1_switch_to_kernel();
-    lab1_print_cur_status();
 }
 
